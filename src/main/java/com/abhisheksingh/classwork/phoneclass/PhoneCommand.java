cc8c831a8616b83e943631159253f95c1f09e7d8
abhishek <abhisingh352000@gmail.com>
2021-10-27 14:58:59 +0300

    Created new task. Added documentation. Added Javadoc comment to PhoneCommand interface

--- src/main/java/com/abhisheksingh/classwork/phoneclass/PhoneCommand.java
+++ src/main/java/com/abhisheksingh/classwork/phoneclass/PhoneCommand.java
@@ -2,6 +2,9 @@ package com.abhisheksingh.classwork.phoneclass;
 
 import javax.validation.constraints.NotNull;
 
+/**
+ * Command interface for all functionalities of a phone
+ */
 public interface PhoneCommand {
     void receiveCall(@NotNull final Phone phone);
 }
